60403bd5f5316a48f5b2588306e969f312a7ec68
Mariano Gonzalez <mariano.gonzalez@baltosoftware.com>
2022-05-26 21:40:54 -0500

    change localhost

--- mic_vad_streaming/test.sh
+++ mic_vad_streaming/test.sh
@@ -2,18 +2,22 @@
 
 set -xe
 
+#sudo /Users/marianogonzalez/balto/development/STT-examples/mic_vad_streaming/env/bin/activate
+
 THIS=$(dirname "$0")
 
 pushd ${THIS}
-  source ../tests.sh
+#  source ../tests.sh
 
-  pip install --user $(get_python_wheel_url "$1")
-  pip install --user -r <(grep -v stt requirements.txt)
+#  pip install --user $(get_python_wheel_url "$1")
+#  pip install --user -r <(grep -v stt requirements.txt)
 
   pulseaudio &
 
-  python mic_vad_streaming.py \
-	  --model $HOME/STT/models/model.tflite \
-	  --scorer $HOME/STT/models/huge-vocab.scorer \
-	  --file $HOME/STT/audio/2830-3980-0043.wav
+  #./model.pbmm doesnt work
+  python3 mic_vad_streaming.py \
+	  --model ./model.tflite  \
+	  --scorer ./kenlm_es.scorer \
+	  --device 0 \
+	  --savewav /tmp
 popd
